171c45d89a3a721e1e4717223dc70b739866bb68
cupidsluvr <roxyburgos96@gmail.com>
2025-07-08 08:21:07 +0000

    vfsfzasfaf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { Client, GatewayIntentBits } = require('discord.js');
+const { getCloseMatches } = require('string-similarity');
 require('dotenv').config();
 
 const client = new Client({
@@ -9,25 +10,8 @@ const client = new Client({
 const PREFIX = '!';
 const LOG_FILE = 'warehouse_logs.json';
 
-// 🚨 FULL ITEM CATEGORIZATION
+// INVENTORY CATEGORIES
 const categories = {
-  GUNS: [
-    'Tommy’s', 'AKs', 'Double Barrel Shotguns', 'Uzis',
-    'MPX', 'Tec', 'WMs', 'VPs'
-  ],
-  EXPLOSIVES: [
-    'Molly’s', 'C4’s'
-  ],
-  AMMO: [
-    '7.62X54', '7.62X39', '9MM', '.45', '12 Gauge Shells'
-  ],
-  ACCESSORIES: [
-    'Pistol Suppressor', 'SMG Suppressor', 'Rifle Suppressor'
-  ]
-};
+  GUNS: ["Tommy’s", "AKs", "Double Barrel Shotguns", "Uz]()
 
-// 🔍 Find category by item name
-const getCategory = (itemName) => {
-  itemName = itemName.toLowerCase();
-  for
 
